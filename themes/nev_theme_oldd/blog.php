c1a25847c660764b391119ef9c1e87e05ad74eb8
Naasirhusain Mirza <naasirhusain79@gmail.com>
2020-02-10 22:25:53 -0500

    Header

--- themes/nev_theme_oldd/blog.php
+++ themes/nev_theme_oldd/blog.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * The header for our theme
+ * The contact for our theme
  *
  * This is the template that displays all of the <head> section and everything up until <div id="content">
  *
@@ -25,34 +25,24 @@
 	<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'sample-theme' ); ?></a>
 
 	<header id="masthead" class="site-header">
-
-	
-		
 		<div class="site-branding">
 			<?php
-			
 			the_custom_logo();
 			if ( is_front_page() && is_home() ) :
 				?>
-				
+				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
 				<?php
 			else :
 				?>
-				
+				<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
 				<?php
 			endif;
 			$sample_theme_description = get_bloginfo( 'description', 'display' );
 			if ( $sample_theme_description || is_customize_preview() ) :
 				?>
-				
+				<p class="site-description"><?php echo $sample_theme_description; /* WPCS: xss ok. */ ?></p>
 			<?php endif; ?>
 		</div><!-- .site-branding -->
-		<!-- /*logo */ -->
-	<a href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home">
-            <img id="logo" src="<?php echo get_template_directory_uri(); ?>/assets/img/freshBeautyLogo2.svg" alt="Logo" width="350px" height="200px" />
-        </a>
-		
-
 
 		<nav id="site-navigation" class="main-navigation">
 			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'sample-theme' ); ?></button>
@@ -63,8 +53,6 @@
 			) );
 			?>
 		</nav><!-- #site-navigation -->
-
-		
 	</header><!-- #masthead -->
 
 	<div id="content" class="site-content">
---
+++ themes/nev_theme_oldd/footer.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * The template for displaying the footer
+ *
+ * Contains the closing of the #content div and all content after.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package Sample_Theme
+ */
+
+?>
+
+	</div><!-- #content -->
+
+	<footer id="colophon" class="site-footer">
+
+	<!-- /*logo */ -->
+	<a href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home">
+        <img id="twitter" src="<?php echo get_template_directory_uri(); ?>/assets/img/twitter.svg" alt="Logo" width="350px" height="200px" />
+	</a>
+	
+	<a href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home">
+        <img id="facebook" src="<?php echo get_template_directory_uri(); ?>/assets/img/facebook.svg" alt="Logo" width="350px" height="200px" />
+	</a>
+	
+	<a href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home">
+        <img id="instagram" src="<?php echo get_template_directory_uri(); ?>/assets/img/instagram.svg" alt="Logo" width="350px" height="200px" />
+    </a>
+		<!-- .site-info -->
+	</footer><!-- #colophon -->
+</div><!-- #page -->
+
+<?php wp_footer(); ?>
+
+</body>
+</html>

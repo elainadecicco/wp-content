1e70a18acf99cf722a0b63105e5030d006e57afb
Nitesh <nitesh.bansal@dcmail.ca>
2020-03-10 09:17:42 -0400

    changed sample theme to nev theme
    name changed

--- themes/nev_theme/template-parts/content-page.php
+++ themes/nev_theme/template-parts/content-page.php
@@ -4,7 +4,7 @@
  *
  * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
  *
- * @package Sample_Theme
+ * @package nev_Theme
  */
 get_header();
 ?>
@@ -14,14 +14,14 @@ get_header();
 		<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
 	</header><!-- .entry-header -->
 
-	<?php sample_theme_post_thumbnail(); ?>
+	<?php nev_theme_post_thumbnail(); ?>
 
 	<div class="entry-content">
 		<?php
 		the_content();
 
 		wp_link_pages( array(
-			'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'sample-theme' ),
+			'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'nev-theme' ),
 			'after'  => '</div>',
 		) );
 		?>
@@ -34,7 +34,7 @@ get_header();
 				sprintf(
 					wp_kses(
 						/* translators: %s: Name of current post. Only visible to screen readers */
-						__( 'Edit <span class="screen-reader-text">%s</span>', 'sample-theme' ),
+						__( 'Edit <span class="screen-reader-text">%s</span>', 'nev-theme' ),
 						array(
 							'span' => array(
 								'class' => array(),

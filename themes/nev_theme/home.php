57cb842301b29d973e2b545be90ffae066828329
Nitesh <nitesh.bansal@dcmail.ca>
2020-03-10 09:27:13 -0400

    changed sample theme naming to nev theme
    home.php

--- themes/nev_theme/home.php
+++ themes/nev_theme/home.php
@@ -41,7 +41,7 @@
 			</a>
 			
 		<nav id="site-navigation" class="main-navigation">
-			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'sample-theme' ); ?></button>
+			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'nev-theme' ); ?></button>
 			<?php
 			wp_nav_menu( array(
 				'theme_location' => 'menu-1',
